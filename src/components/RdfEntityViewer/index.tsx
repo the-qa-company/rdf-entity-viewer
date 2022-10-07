fa4af16211a1caea483ae37343e039204fb7f11a
Clement Defretiere <clement.def.7@gmail.com>
2022-10-07 14:59:48 +0200

    Project setup

---
+++ src/components/RdfEntityViewer/index.tsx
@@ -0,0 +1,12 @@
+import { Button, Typography } from '@mui/material'
+
+function RdfEntityViewer (): JSX.Element {
+  return (
+    <div>
+      <Typography variant='h1'>RdfEntityViewer</Typography>
+      <Button>Example Button</Button>
+    </div>
+  )
+}
+
+export default RdfEntityViewer
