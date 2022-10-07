6c0fc5627b3391f04fb9d17c19d226c10d1589c3
Clement Defretiere <clement.def.7@gmail.com>
2022-10-07 16:32:04 +0200

    ci

---
+++ scripts/prepare-package.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This script is used to backup package.json file
+# and modify it so that it can be used to publish
+
+# Backup package.json
+cp package.json package.json.bak
+
+# Edit package.json
+npx json -I -f package.json -e "this.devDependencies = {}"
