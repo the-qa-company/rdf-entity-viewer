8ad22ffe1ed2adc77f61e1ee672860355420c4c1
Clement Defretiere <defretiere.c@gmail.com>
2022-12-07 15:24:31 +0100

    Fix label resolution (added fallbacks)

--- src/lib/format.ts
+++ src/lib/format.ts
@@ -1,3 +1,4 @@
+import { Object as RdfObject } from './rdf-json'
 import { ViewerContextI } from './viewer-context'
 
 export const formatIRI = (viewerCtx: ViewerContextI, iri: string): string => {
@@ -26,18 +27,33 @@ export const formatIRI = (viewerCtx: ViewerContextI, iri: string): string => {
 
 export const getLabel = (viewerCtx: ViewerContextI, iri: string): string | undefined => {
   const { data, labelIRIs, locale } = viewerCtx
+  // No data => no label
   if (data === undefined) return undefined
+  // Get the predicates for the IRI
   const predicates = data[iri]
   if (predicates === undefined) return undefined
+  // Go through the label IRIs and find the best label possible
+  let bestLabel: RdfObject | undefined
+  let bestLabelCondition: 'no-lang' | 'en-fallback' | undefined // Store the condition that made the label the best
   for (const labelIRI of labelIRIs) {
     const labels = predicates[labelIRI]
     if (labels !== undefined) {
       for (const label of labels) {
         if (label.lang === locale.language) {
-          return label.value
+          return label.value // Best match
+        } else if (label.lang === undefined) {
+          if (bestLabelCondition === undefined || bestLabelCondition === 'en-fallback') {
+            bestLabel = label // No-lang match (2st best)
+            bestLabelCondition = 'no-lang'
+          }
+        } else if (label.lang === 'en' && bestLabel === undefined) {
+          if (bestLabelCondition === undefined) {
+            bestLabel = label // English match (3rd best)
+            bestLabelCondition = 'en-fallback'
+          }
         }
       }
     }
   }
-  return undefined
+  return bestLabel?.value
 }
